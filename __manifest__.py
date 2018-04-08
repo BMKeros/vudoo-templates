08051accbf89a7dd8a87d3c0083b0b4b2e90e8a1
Israel Lugo <hostelixisrael@gmail.com>
2018-04-08 16:56:33 -0400

    [MOD] change module => module_name

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "{{module}}",
+    'name': "{{module_name}}",
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
