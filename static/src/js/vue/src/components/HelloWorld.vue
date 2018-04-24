4d7014141cbc41fefd794bc4cadf535642316619
danzt <danztty@gmail.com>
2018-04-24 17:32:00 -0400

    [UPD]Modificado la plantilla agregado semantic

--- static/src/js/vue/src/components/HelloWorld.vue
+++ static/src/js/vue/src/components/HelloWorld.vue
@@ -1,15 +1,35 @@
 <template>
 	<div>
-		<p>New Module {{module_name}}</p>
-		<footer>
-			<span>Was created by {{'author'}}</span>
-		</footer>
+		<sui-grid celled>
+			<sui-grid-row>
+				<sui-grid-column :width="4">
+					<sui-message icon="folder"
+								 header="Se a creado el modulo {{module_name}} Con exito!"
+								 content="Usando Vuex integrado con Odoo."/>
+				</sui-grid-column>
+			</sui-grid-row>
+			
+			<sui-grid-row>
+				<sui-grid-column :width="4">
+					<sui-card>
+						<sui-card-content :width="4">
+							<sui-card-header>Module creado fue creado por {{author}}
+								<sui-icon class="right floated" size="small" name="star" />
+							</sui-card-header>
+							<sui-card-description>
+								<p>{{description}}</p>
+							</sui-card-description>
+						</sui-card-content>
+					</sui-card>
+				</sui-grid-column>
+			</sui-grid-row>
+		</sui-grid>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'hello-world'
+		name: 'hello-world',
 	}
 </script>
 
--- static/src/js/vue/src/main.js
+++ static/src/js/vue/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld';
 import store from './store'
+import SuiVue from 'semantic-ui-vue';
 
+
+Vue.use(SuiVue);
 Vue.component('hello-world', HelloWorld);
 
 new Vue({
