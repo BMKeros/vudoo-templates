581620af0592255ef74669cd24ae26592049c41d
danzt <danztty@gmail.com>
2018-04-30 18:49:11 -0400

    [UPD]modificado el nombre del component

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -16,7 +16,7 @@
 
 <script>
 	export default {
-		name: 'hello-world',
+		name: 'home',
 	}
 </script>
 
--- static/src/js/vue/src/main.js
+++ static/src/js/vue/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/Home';
 import store from './store'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
-Vue.component('hello-world', HelloWorld);
+Vue.component('home', Home);
 
 new Vue({
 	el: '#app',
   	store,
-	render: h => h(HelloWorld)
+	render: h => h(Home)
 });
