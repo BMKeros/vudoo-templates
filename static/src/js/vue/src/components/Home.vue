8e8f7c6e048148a71cf88909513d33ad94d0ea2d
danzt <danztty@gmail.com>
2018-04-30 18:57:34 -0400

    [UPD]agregada las variables dinamicas al template

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -3,11 +3,11 @@
 		<v-container fill-height>
 			<v-layout align-center>
 				<v-flex>
-					<h3 class="display-3">Welcome to the site</h3>
-					<span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+					<h3 class="display-3">Welcome to the site {{module_name}}</h3>
+					<span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum. {{description}}</span>
 					<v-divider class="my-3"></v-divider>
-					<div class="title mb-3">Check out our newest features!</div>
-					<v-btn large color="primary" class="mx-0">See more</v-btn>
+					<div class="title mb-3">Check out our newest features! {{license}}</div>
+					<v-btn large color="primary" class="mx-0">{{author}}</v-btn>
 				</v-flex>
 			</v-layout>
 		</v-container>
