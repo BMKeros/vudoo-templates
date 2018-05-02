6481029c78370b221490566f3d6a2aa196e28c61
José Visliquez <jsxd.17@gmail.com>
2018-05-02 00:27:01 -0400

    [UPD]: - url-loader added to rules array in webpack configuration file - Semantic CSS CDN removed from assets.xml

--- static/src/js/vue/webpack.config.js
+++ static/src/js/vue/webpack.config.js
@@ -9,88 +9,96 @@ const inProjectSrc = (file) => inProject(project.srcDir, file);
 const __PROD__ = project.env === 'production';
 
 const webpackConfig = {
-    mode: project.env,
-    entry: {
-        main: [
-            inProjectSrc(project.main)
-        ]
-    },
-    module: {
-        rules: []
-    },
-    devtool: __PROD__ ? 'source-map' : 'cheap-eval-source-map',
-    resolve: {
-        modules: [
-            path.resolve('/'),
-            'node_modules'
-        ],
-        extensions: ['*','.js','.vue','.json']
-    },
-    plugins:[
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_END: JSON.stringify(project.env)
-            }
-        })
-    ],
-    output: {
-        filename: __PROD__ ? 'build.min.js' : 'build.js',
-        path: path.resolve(project.outDir),
-        publicPath: project.publicPath
-    }
+	mode: project.env,
+	entry: {
+		main: [
+			inProjectSrc(project.main)
+		]
+	},
+	module: {
+		rules: []
+	},
+	devtool: __PROD__ ? 'source-map' : 'cheap-eval-source-map',
+	resolve: {
+		modules: [
+			path.resolve('/'),
+			'node_modules'
+		],
+		extensions: ['*','.js','.vue','.json']
+	},
+	plugins:[
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_END: JSON.stringify(project.env)
+			}
+		})
+	],
+	output: {
+		filename: __PROD__ ? 'build.min.js' : 'build.js',
+		path: path.resolve(project.outDir),
+		publicPath: project.publicPath
+	}
 };
 
 //Rules
 webpackConfig.module.rules.push(
-    {
-        test: /\.css$/,
-        use: [
-            'vue-style-loader',
-            'css-loader'
-        ]
-    },
-    {
-        test: /\.vue$/,
-        use: 'vue-loader',
-    },
-    {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-    }
+	{
+		test: /\.js$/,
+		use: 'babel-loader',
+		exclude: /node_modules/,
+	},
+	{
+		test: /\.vue$/,
+		use: 'vue-loader',
+	},
+	{
+		test: /\.css$/,
+		use: [
+			'vue-style-loader',
+			'css-loader'
+		]
+	},
+	{
+		test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+		loader: 'url-loader',
+	},
+	{
+		test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+		loader: 'url-loader',
+	}
 );
 
 if(__PROD__){
-    webpackConfig.plugins.push(
-        new UglifyJsPlugin({
-            cache: true,
-            parallel: true,
-            sourceMap: true,
-            uglifyOptions: {
-                compress: {
-                    conditionals: true,
-                    unused: true,
-                    comparasons: true,
-                    sequences: true,
-                    dead_code: true,
-                    if_return: true,
-                    join_vars: true,
-                    topLevel: true,
-                    passes: 1
-                },
-                comments: false,
-                warnings: false,
-                toplevel: true,
-                nameCache: null,
-                ie8: false,
-                keep_classnames: undefined,
-                keep_fnames: false,
-                safari10: false,
-                mangle: true
-            },
-            warningsFilter: (src) => true
-        })
-    )
+	webpackConfig.plugins.push(
+		new UglifyJsPlugin({
+			cache: true,
+			parallel: true,
+			sourceMap: true,
+			uglifyOptions: {
+				compress: {
+					conditionals: true,
+					unused: true,
+					comparasons: true,
+					sequences: true,
+					dead_code: true,
+					if_return: true,
+					join_vars: true,
+					toplevel: true,
+					passes: 1
+				},
+				comments: false,
+				warnings: false,
+				toplevel: true,
+				nameCache: null,
+				ie8: false,
+				keep_classnames: undefined,
+				keep_fnames: false,
+				safari10: false,
+				mangle: true
+			},
+			warningsFilter: (src) => true
+		})
+	)
 };
 
 module.exports = webpackConfig;
