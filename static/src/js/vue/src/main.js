057bf03d935a9b6fe3794721ee3fc88dd1fd9507
danzt <danztty@gmail.com>
2018-05-02 09:37:19 -0400

    [UPD]change HelloWorld to Home

--- static/src/js/vue/src/main.js
+++ static/src/js/vue/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld';
+import Home from './components/Home';
 import store from './store'
 import SuiVue from 'semantic-ui-vue';
 import 'semantic-ui-css/semantic.min.css';
 
 Vue.use(SuiVue);
-Vue.component('hello-world', HelloWorld);
+Vue.component('home', Home);
 
 new Vue({
 	el: '#app',
   	store,
-	render: h => h(HelloWorld)
+	render: h => h(Home)
 });
