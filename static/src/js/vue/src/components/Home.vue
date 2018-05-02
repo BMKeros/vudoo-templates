93bfe37902bda56cc021633a5a702a3f07c2dab6
danzt <danztty@gmail.com>
2018-05-02 10:12:51 -0400

    [UPD]Add commit lema

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -7,25 +7,27 @@
 						<sui-grid-row>
 							<sui-grid-column :width="13" centered>
 								<sui-message icon="folder"
-											header="Se a creado el modulo {{module_name}} Con exito!"
-											content="Usando Vuex integrado con Odoo."/>
+											header=" The {{module_name}} has been created successfully!"
+											content="Using Vuex integrated with Odoo."/>
 							</sui-grid-column>
 						</sui-grid-row>
 						<sui-grid-row>
 							<sui-grid-column centered :width="8">
 								<sui-card>
 									<sui-card-content :width="8">
-										<sui-card-header>Y este modulo fue creado por {{author}}
+										<sui-card-header>This module was created by: {{author}}
 											<sui-icon class="right floated" size="small" name="star" />
 										</sui-card-header>
 										<sui-card-description>
 											<p>{{description}}</p>
 										</sui-card-description>
-
 										<sui-card-meta>
 											<p>{{license}}</p>
 										</sui-card-meta>
 									</sui-card-content>
+                                    <div class="extra content">
+                                        <span class="right floated">Made with the heart</span>
+                                    </div>
 								</sui-card>
 							</sui-grid-column>
 						</sui-grid-row>
