8fb044d4a1697c75b1ad3fc951ba3efdca0d0185
Israel Lugo <hostelixisrael@gmail.com>
2018-05-02 17:50:16 -0400

    [UPD] add font google

--- static/src/js/vue/src/main.js
+++ static/src/js/vue/src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify'
 import Home from './components/Home';
-import store from './store'
-import 'vuetify/dist/vuetify.min.css'
+import store from './store';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify);
 Vue.component('demo', Home);
 
 new Vue({
-	el: '#app',
-  	store,
-	render: h => h(Home)
+    el: '#app',
+    store,
+    render: h => h(Home)
 });
