788c4819c8ea9fdbe7049ae3358a993808001e9e
danzt <danztty@gmail.com>
2018-05-02 17:34:24 -0400

    [FIX] agregado alias al webpack

--- static/src/js/vue/webpack.config.js
+++ static/src/js/vue/webpack.config.js
@@ -9,96 +9,100 @@ const inProjectSrc = (file) => inProject(project.srcDir, file);
 const __PROD__ = project.env === 'production';
 
 const webpackConfig = {
-    mode: project.env,
-    entry: {
-        main: [
-            inProjectSrc(project.main)
-        ]
-    },
-    module: {
-        rules: []
-    },
-    devtool: __PROD__ ? 'source-map' : 'cheap-eval-source-map',
-    resolve: {
-        modules: [
-            path.resolve('/'),
-            'node_modules'
-        ],
-        extensions: ['*','.js','.vue','.json']
-    },
-    plugins:[
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_END: JSON.stringify(project.env)
-            }
-        })
+  mode: project.env,
+  entry: {
+    main: [
+      inProjectSrc(project.main)
+    ]
+  },
+  module: {
+    rules: []
+  },
+  devtool: __PROD__ ? 'source-map' : 'cheap-eval-source-map',
+  resolve: {
+    modules: [
+      path.resolve('/'),
+      'node_modules'
     ],
-    output: {
-        filename: __PROD__ ? 'build.min.js' : 'build.js',
-        path: path.resolve(project.outDir),
-        publicPath: project.publicPath
+    extensions: ['*', '.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
     }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_END: JSON.stringify(project.env)
+      }
+    })
+  ],
+  output: {
+    filename: __PROD__ ? 'build.min.js' : 'build.js',
+    path: path.resolve(project.outDir),
+    publicPath: project.publicPath
+  }
 };
 
 //Rules
 webpackConfig.module.rules.push(
-	{
-		test: /\.js$/,
-		use: 'babel-loader',
-		exclude: /node_modules/,
-	},
-	{
-		test: /\.vue$/,
-		use: 'vue-loader',
-	},
-	{
-		test: /\.css$/,
-		use: [
-			'vue-style-loader',
-			'css-loader'
-		]
-	},
-	{
-		test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-		loader: 'url-loader',
-	},
-	{
-		test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-		loader: 'url-loader',
-	}
+  {
+    test: /\.js$/,
+    use: 'babel-loader',
+    exclude: /node_modules/,
+  },
+  {
+    test: /\.vue$/,
+    use: 'vue-loader',
+  },
+  {
+    test: /\.css$/,
+    use: [
+      'vue-style-loader',
+      'css-loader'
+    ]
+  },
+  {
+    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+    loader: 'url-loader',
+  },
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: 'url-loader',
+  }
 );
 
-if(__PROD__){
-    webpackConfig.plugins.push(
-        new UglifyJsPlugin({
-            cache: true,
-            parallel: true,
-            sourceMap: true,
-            uglifyOptions: {
-                compress: {
-                    conditionals: true,
-                    unused: true,
-                    comparasons: true,
-                    sequences: true,
-                    dead_code: true,
-                    if_return: true,
-                    join_vars: true,
-                    topLevel: true,
-                    passes: 1
-                },
-                comments: false,
-                warnings: false,
-                toplevel: true,
-                nameCache: null,
-                ie8: false,
-                keep_classnames: undefined,
-                keep_fnames: false,
-                safari10: false,
-                mangle: true
-            },
-            warningsFilter: (src) => true
-        })
-    )
-};
+if (__PROD__) {
+  webpackConfig.plugins.push(
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        compress: {
+          conditionals: true,
+          unused: true,
+          comparasons: true,
+          sequences: true,
+          dead_code: true,
+          if_return: true,
+          join_vars: true,
+          topLevel: true,
+          passes: 1
+        },
+        comments: false,
+        warnings: false,
+        toplevel: true,
+        nameCache: null,
+        ie8: false,
+        keep_classnames: undefined,
+        keep_fnames: false,
+        safari10: false,
+        mangle: true
+      },
+      warningsFilter: (src) => true
+    })
+  )
+}
+;
 
 module.exports = webpackConfig;
