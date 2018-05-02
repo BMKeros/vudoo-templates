0d2906abb0e9850753fe349bb48e6544ff59add1
José Visliquez <jsxd.17@gmail.com>
2018-05-02 00:42:25 -0400

    [UPD]: - semantic-ui-css import added to main.js, rearranged devDependencies and dependencies.

--- static/src/js/vue/src/main.js
+++ static/src/js/vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import HelloWorld from './components/HelloWorld';
 import store from './store'
 import SuiVue from 'semantic-ui-vue';
-
+import 'semantic-ui-css/semantic.min.css';
 
 Vue.use(SuiVue);
 Vue.component('hello-world', HelloWorld);
