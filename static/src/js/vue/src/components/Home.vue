90f139ac0268f4e2f086b8c6a9574186d21182ca
danzt <danztty@gmail.com>
2018-05-02 17:12:04 -0400

    [FIX]reparado error de los font

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-jumbotron color="grey lighten-2">
-    <v-container fill-height>
-      <v-layout align-center>
-        <v-flex>
-          <h3 class="display-3">Welcome to the site {{module_name}}</h3>
-          <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum. {{description}}</span>
-          <v-divider class="my-3"></v-divider>
-          <div class="title mb-3">Check out our newest features! {{license}}</div>
-          <v-btn large color="primary" class="mx-0">{{author}}</v-btn>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-jumbotron>
+  <v-app>
+    <v-jumbotron color="grey lighten-2">
+      <v-container fill-height>
+        <v-layout align-center>
+          <v-flex>
+            <h3 class="display-3">The {{module_name}} has been created successfully!</h3>
+            <span class="subheading">Using Vuex integrated with Odoo. {{description}}</span>
+            <v-divider class="my-3"></v-divider>
+            <div class="title mb-3">This module was created by: {{author}}</div>
+            <v-btn large color="primary" class="mx-0">{{license}}</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-jumbotron>
+  </v-app>
 </template>
 
 <script>
