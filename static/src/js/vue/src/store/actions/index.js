b07ae1cbc74ac196d82f338ca486ed5979946d7b
Israel Lugo <hostelixisrael@gmail.com>
2018-05-04 12:39:01 -0400

    [UPD] add actions saveTask and closeDialog

--- static/src/js/vue/src/store/actions/index.js
+++ static/src/js/vue/src/store/actions/index.js
@@ -3,13 +3,25 @@ import { Model } from 'odoo-webkit';
 const Task = new Model('todo.task');
 
 const actions = {
-    createTask: ({ commit }, data) => {
-        Task.create(data).then(result => dispatch('loadTasks'));
+    saveTask: ({ commit, dispatch }, payload) => {
+        const { data, action } = payload;
+        if (action === 'create') {
+            Task.create(data).then(result => {
+                dispatch('loadTasks');
+                commit('TOGGLE_DIALOG');
+            });
+        }
+        else if (action === 'edit') {
+            Task.write(data.id, data).then(result => {
+                dispatch('loadTasks');
+                commit('TOGGLE_DIALOG');
+            });
+        }
     },
     editTask: ({ commit, dispatch }, data) => {
         commit('LOAD_EDITED_TASK', data);
+        commit('CHANGE_ACTION_FORM', 'edit');
         dispatch('toggleDialog');
-        //Task.write(data).then(result => dispatch('loadTasks'));
     },
     deleteTask: ({ dispatch }, data) => {
         if (confirm('Are you sure you want to delete this task?')) {
@@ -22,6 +34,11 @@ const actions = {
     toggleDialog: ({ commit }) => {
         commit('TOGGLE_DIALOG');
     },
+    closeDialog: ({ commit }) => {
+        commit('CLEAR_FORM');
+        commit('TOGGLE_DIALOG');
+        commit('CHANGE_ACTION_FORM', 'create');
+    }
 };
 
 export default actions;
