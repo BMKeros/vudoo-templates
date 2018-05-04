afee62bc64e67b8f424eb64ffe1dac80f2790d69
danzt <danztty@gmail.com>
2018-05-04 11:39:16 -0400

    [UPD2][FIX]reparada indentacion en el package.json

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -34,36 +34,39 @@
           justify-center
           align-center
         >
-          <v-jumbotron color="grey lighten-2">
-            <v-container fill-height>
-              <v-layout align-center>
-                <v-flex>
-                  <h3 class="display-3">Welcome to the site</h3>
-                  <span class="subheading"> This Module was created by {{author}}!</span>
-                  <v-divider class="my-3"><p>Under the {{license}} license</p></v-divider>
-                  <div class="title mb-3">Check out our newest features!</div>
-                  <v-btn large color="primary" class="mx-0">See more</v-btn>
-                </v-flex>
-                <v-flex>
-                  <v-dialog v-model="dialog" width="600px">
-                    <v-btn color="primary" dark slot="activator">Open Details</v-btn>
-                    <v-card>
-                      <v-card-title>
-                        <span class="headline">{{module_name}}</span>
-                      </v-card-title>
-                      <v-card-text>{{description}}
-                        <p>You can get the repository on Github {{git_repository}}</p>
-                      </v-card-text>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="green darken-1" flat="flat" @click="dialog = false">Close</v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-jumbotron>
+          <v-flex>
+            <v-jumbotron color="grey lighten-2">
+              <v-container fill-height>
+                <v-layout align-center>
+                  <v-flex>
+                    <h3 class="display-3">Welcome to Odoo WebSite</h3>
+                    <span class="subheading"></span>
+                    <v-divider class="my-3"></v-divider>
+                    <div class="title mb-3">This Module was created by {{author}}!
+                      <p>Under the {{license}} license</p>
+                    </div>
+                    <v-layout row justify-center>
+                      <v-dialog v-model="dialog" width="600px">
+                        <v-btn color="primary" dark slot="activator">Open Details</v-btn>
+                        <v-card>
+                          <v-card-title>
+                            <span class="headline">{{module_name}}</span>
+                          </v-card-title>
+                          <v-card-text>{{description}}
+                            <p>You can get the repository on Github {{git_repository}}</p>
+                          </v-card-text>
+                          <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="green darken-1" flat="flat" @click="dialog = false">Close</v-btn>
+                          </v-card-actions>
+                        </v-card>
+                      </v-dialog>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-jumbotron>
+          </v-flex>
         </v-layout>
       </v-container>
     </v-content>
@@ -88,6 +91,9 @@
       drawer: null,
       dialog: false
     }),
+    props: {
+      source: String
+    }
   }
 </script>
 
