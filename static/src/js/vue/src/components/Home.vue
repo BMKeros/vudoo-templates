ff68b31d86b0f3f73bd76d2d28d6274b6d3ed17a
Israel Lugo <hostelixisrael@gmail.com>
2018-05-04 12:37:10 -0400

    [UPD] new changes in save and edit task

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -4,7 +4,7 @@
         <v-dialog v-model="dialog" max-width="500px" persistent>
             <v-card>
                 <v-card-title>
-                    <span class="headline">\{{ formTitle }}</span>
+                    <span class="headline">{{ formTitle }}</span>
                 </v-card-title>
                 <v-card-text>
                     <v-container grid-list-md>
@@ -16,7 +16,28 @@
                                 <v-text-field label="User" v-model="editedTask.user"></v-text-field>
                             </v-flex>
                             <v-flex xs12 sm6 md4>
-                                <v-text-field label="Date Deadline" v-model="editedTask.date_deadline"></v-text-field>
+                                <v-dialog
+                                        ref="dialog"
+                                        persistent
+                                        v-model="modal"
+                                        lazy
+                                        :return-value.sync="editedTask.date_deadline"
+                                >
+                                    <v-text-field
+                                            slot="activator"
+                                            label="Date Deadline"
+                                            v-model="editedTask.date_deadline"
+                                            prepend-icon="event"
+                                            readonly
+                                    ></v-text-field>
+                                    <v-date-picker v-model="editedTask.date_deadline">
+                                        <v-spacer></v-spacer>
+                                        <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
+                                        <v-btn flat color="primary"
+                                               @click="$refs.dialog.save(editedTask.date_deadline)">OK
+                                        </v-btn>
+                                    </v-date-picker>
+                                </v-dialog>
                             </v-flex>
                             <v-flex xs12 sm6 md4>
                                 <v-checkbox label="Done?" v-model="editedTask.is_done"></v-checkbox>
@@ -26,8 +47,10 @@
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" flat @click.native="toggleDialog">Close</v-btn>
-                    <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="closeDialog">Close</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="saveTask({data: editedTask, action: currentAction})">
+                        Save
+                    </v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -38,13 +61,13 @@
                 class="elevation-1"
         >
             <template slot="items" slot-scope="props">
-                <td>\{{ props.item.id }}</td>
-                <td class="text-xs-right">\{{ props.item.name }}</td>
-                <td class="text-xs-right">\{{ props.item.user }}</td>
-                <td class="text-xs-right">\{{ props.item.date_deadline }}</td>
+                <td>{{ props.item.id }}</td>
+                <td class="text-xs-right">{{ props.item.name }}</td>
+                <td class="text-xs-right">{{ props.item.user }}</td>
+                <td class="text-xs-right">{{ props.item.date_deadline }}</td>
                 <td class="text-xs-right">
-                    <v-icon v-if="props.item.is_done" color="teal">check</v-icon>
-                    <v-icon v-else color="teal">clear</v-icon>
+                    <v-icon v-if="props.item.is_done" color="green">check</v-icon>
+                    <v-icon v-else color="red">clear</v-icon>
                 </td>
                 <td class="justify-center layout px-0">
                     <v-btn icon class="mx-0" @click="editTask(props.item)">
@@ -54,9 +77,6 @@
                         <v-icon color="pink">delete</v-icon>
                     </v-btn>
                 </td>
-            </template>
-            <template slot="no-data">
-                <v-btn color="primary" @click="initialize">Reset</v-btn>
             </template>
         </v-data-table>
     </v-app>
@@ -69,23 +89,25 @@
         name: 'demo',
         data: () => ({
             headers: [
-                { text: 'User', value: 'user' },
+                { text: 'ID', value: 'id' },
                 { text: 'Name', value: 'name' },
+                { text: 'User', value: 'user' },
                 { text: 'Date Deadline', value: 'date_deadline' },
                 { text: 'Done?', value: 'is_done' },
                 { text: 'Actions', value: '', sortable: false }
-            ]
+            ],
+            modal: false
         }),
         computed: {
             formTitle() {
-                //return this.editedIndex === -1 ? 'New Task' : 'Edit Task'
-                return 'New Task';
+                return this.currentAction === 'create' ? 'New Task' : 'Edit Task'
             },
             ...mapState([
                 'dialog',
                 'tasks',
                 'editedTask',
-                'defaultTask'
+                'defaultTask',
+                'currentAction'
             ])
         },
         mounted() {
@@ -93,13 +115,13 @@
         },
         methods: {
             ...mapActions([
-                'createTask',
+                'saveTask',
                 'editTask',
-                'removeTask',
+                'deleteTask',
                 'loadTasks',
-                'toggleDialog'
-            ]),
-            initialize: () => console.log("init"),
+                'toggleDialog',
+                'closeDialog'
+            ])
         }
     }
 </script>
