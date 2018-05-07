7f104c4896f302693813fe18200430fca362faae
danzt <danztty@gmail.com>
2018-05-07 15:51:38 -0400

    [UPD8][FIX]

--- static/src/js/vue/src/components/Home.vue
+++ static/src/js/vue/src/components/Home.vue
@@ -28,48 +28,43 @@
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>{{module_name}}</v-toolbar-title>
     </v-toolbar>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout
-          justify-center
-          align-center
-        >
-          <v-flex>
-            <v-jumbotron color="grey lighten-2">
-              <v-container>
-                <v-layout align-center>
-                  <v-flex>
-                    <span class="subheading"></span>
-                    <div class="title mb-6">
-                      <h3 class="display-3">Welcome to Odoo WebSite</h3>
-                      This Module was created by {{author}}!
-                      <p>Under the {{license}} license</p>
-                    </div>
-                    <v-layout row justify-center>
-                      <v-dialog v-model="dialog" width="600px">
-                        <v-btn color="primary" dark slot="activator">Open Details</v-btn>
-                        <v-card>
-                          <v-card-title>
-                            <span class="headline">{{module_name}}</span>
-                          </v-card-title>
-                          <v-card-text>{{description}}
-                            <p>You can get the repository on Github {{git_repository}}</p>
-                          </v-card-text>
-                          <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="green darken-1" flat="flat" @click="dialog = false">Close</v-btn>
-                          </v-card-actions>
-                        </v-card>
-                      </v-dialog>
-                    </v-layout>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-jumbotron>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+    <v-container>
+      <v-layout align-baseline>
+        <v-flex>
+          <v-jumbotron color="grey lighten-2">
+            <v-container>
+              <v-layout align-center>
+                <v-flex xs10 offset-xs1>
+                  <span class="subheading"></span>
+                  <div class="title mb-6">
+                    <h3 class="display-3">Welcome to Odoo WebSite</h3>
+                    This Module was created by {{author}}!
+                    <p>Under the {{license}} license</p>
+                  </div>
+                  <v-layout row justify-center>
+                    <v-dialog v-model="dialog" width="600px">
+                      <v-btn color="primary" dark slot="activator">Open Details</v-btn>
+                      <v-card>
+                        <v-card-title>
+                          <span class="headline">{{module_name}}</span>
+                        </v-card-title>
+                        <v-card-text>{{description}}
+                          <p>You can get the repository on Github {{git_repository}}</p>
+                        </v-card-text>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="green darken-1" flat="flat" @click="dialog = false">Close</v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </v-layout>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-jumbotron>
+        </v-flex>
+      </v-layout>
+    </v-container>
     <v-container grid-list-xl text-xs-center>
       <v-flex xs10 offset-xs1>
         <v-footer color="indigo" app>
